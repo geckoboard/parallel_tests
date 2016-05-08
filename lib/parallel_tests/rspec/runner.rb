2c944df58ac42ae5f0d2524a2baf4d6950329d68
Jon Normington <jnormington@users.noreply.github.com>
2016-05-08 15:31:45 +0000

    Start a new xfvb sessio for each rspec call. As we rely on the mouse placement using just one xfvb session caused mouse issues starting a new display for each resolves this

--- lib/parallel_tests/rspec/runner.rb
+++ lib/parallel_tests/rspec/runner.rb
@@ -10,7 +10,9 @@ module ParallelTests
         def run_tests(test_files, process_number, num_processes, options)
           exe = executable # expensive, so we cache
           version = (exe =~ /\brspec\b/ ? 2 : 1)
-          cmd = [exe, options[:test_options], (rspec_2_color if version == 2), spec_opts, *test_files].compact.join(" ")
+          cmd = [xvfb_session_cmd(process_number), exe, options[:test_options],
+                 (rspec_2_color if version == 2), spec_opts, *test_files].compact.join(" ")
+
           options = options.merge(:env => rspec_1_color) if version == 1
           execute_command(cmd, process_number, num_processes, options)
         end
@@ -29,6 +31,10 @@ module ParallelTests
           end
 
           cmd or raise("Can't find executables rspec or spec")
+        end
+
+        def xvfb_session_cmd(process_number)
+          "xvfb-run -n #{process_number}"
         end
 
         def runtime_log
